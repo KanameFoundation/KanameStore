6ac85c6fed1ba663783df8a316a30d9fe1232b7c
DemuraAIdev <cyberaioff@gmail.com>
2026-01-16 23:02:36 +0700

    AceEditor

---
+++ apps/AceEditor/rspack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const rspack = require("@rspack/core");
+const mode = process.env.NODE_ENV || "development";
+const minimize = mode === "production";
+
+module.exports = {
+    mode,
+    devtool: "source-map",
+    entry: {
+        main: [
+            path.resolve(__dirname, "index.js"),
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
+        library: "AceEditor",
+        libraryTarget: "umd",
+    },
+    externals: {
+        osjs: "OSjs",
+    },
+    optimization: {
+        minimize,
+    },
+    plugins: [
+        new rspack.CssExtractRspackPlugin({
+            filename: "[name].css",
+        }),
+        new rspack.CopyRspackPlugin({
+            patterns: [
+                { from: "icon.png" }
+            ]
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "builtin:swc-loader",
+                    options: {
+                        jsc: {
+                            parser: {
+                                syntax: "ecmascript",
+                            },
+                        },
+                    },
+                },
+            },
+        ],
+    },
+};
